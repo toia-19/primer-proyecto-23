93672511b5fb169b7e420e40c62b8b8c437fa8c4
toia-19 <vickingiediaz19@gmail.com>
2023-08-15 11:05:48 -0300

    agregando diseño a inputs de register; agregando alertas para register

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -17,18 +17,26 @@ export class RegisterComponent {
   }
 
   // servicioAuth referencia a nuestro servicio Auth
-  constructor(public servicioAuth: AuthService){
+  constructor(public servicioAuth: AuthService) {
   }
 
   // tomamos nuevos registros y mostramos los resultados
-  async registrarse(){
+  async registrarse() {
     const credenciales = {
       nombre: this.usuarios.nombre,
       contrasena: this.usuarios.contrasena
     };
 
-    const res = await this.servicioAuth.registrar(credenciales.nombre,credenciales.contrasena).catch(error => {console.log('error =>',error)});
-    
+    const res = await this.servicioAuth.registrar(credenciales.nombre, credenciales.contrasena)
+    // el método THEN nos devuelve el mismo valor que guarda la promesa
+    .then(res =>{
+      alert("Ha agregado un nuevo usuario con éxito :)");
+    })
+    // el método CATCH creará un error en caso de que las cosas salgan mal
+    .catch(error => 
+      alert("Hubo un error al cargar el usuario :( \n"+error)
+    );
+
     console.log(res);
   }
 }
